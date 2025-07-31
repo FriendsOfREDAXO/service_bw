56a9b72152c9bfea7deb6bfd8045a4b2cddd0852
christophboecker <christoph_boecker@t-online.de>
2025-07-31 14:01:29 +0200

    Migrationsklassen mit altem Namespace

---
+++ lib/transition.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Hilfsweise für die Umstellung die Klasse im alten Namespace zur Verfügung stellen.
+ *
+ * @see https://github.com/orgs/FriendsOfREDAXO/discussions/40
+ */
+
+namespace ServiceBw;
+
+/**
+ * @deprecated 2.0.0 Aufrufe auf "FriendsOfRedaxo\ServiceBw\Api" (neuer Namespace) umstellen
+ */
+class Api extends \FriendsOfRedaxo\ServiceBw\Api {}
+
+/**
+ * @deprecated 2.0.0 Aufrufe auf "FriendsOfRedaxo\ServiceBw\View" (neuer Namespace) umstellen
+ */
+class View extends \FriendsOfRedaxo\ServiceBw\View {}
